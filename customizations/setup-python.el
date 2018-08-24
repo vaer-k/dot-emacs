a9e6a683e229681dba72361d7d319a8659d8f10d
Vincent Raerek <vincent@vaer-k.net>
2018-08-24 16:57:42 -0700

    Update setup-python.el

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -2,6 +2,10 @@
 ;; Warning (python): Your ‘python-shell-interpreter’ doesn’t seem to support readline, yet ‘python-shell-completion-native’ was t and "ipython3" is not part of the ‘python-shell-completion-native-disabled-interpreters’ list. Native completions have been disabled locally.
 ;; It appears this issue will be resolved in an upcoming version of emacs or elpy
 ;; https://github.com/jorgenschaefer/elpy/issues/887
+
+;; NOTE: You will also be required to install some python packages via pip for elpy to work properly. 
+;; Run M-x elpy-config for details
+
 (setq python-shell-completion-native-enable nil)
 
 (setq python-shell-interpreter "python3.7")
