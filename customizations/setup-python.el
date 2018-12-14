f9d9f5ce2b56b5c8f85e00bc3aba6d4e873aa996
Vincent Raerek <vincent@vaer-k.net>
2018-12-14 11:03:06 -0800

    Update setup-python.el

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -1,5 +1,10 @@
+;;!important -- You must install required dependencies with pip before elpy will
+;; function properly: https://github.com/jorgenschaefer/elpy
+
 ;; The following suppresses a warning about readline not being supported:
-;; Warning (python): Your ‘python-shell-interpreter’ doesn’t seem to support readline, yet ‘python-shell-completion-native’ was t and "ipython3" is not part of the ‘python-shell-completion-native-disabled-interpreters’ list. Native completions have been disabled locally.
+;; Warning (python): Your ‘python-shell-interpreter’ doesn’t seem to support readline, 
+;; yet ‘python-shell-completion-native’ was t and "ipython3" is not part of the 
+;; ‘python-shell-completion-native-disabled-interpreters’ list. Native completions have been disabled locally.
 ;; It appears this issue will be resolved in an upcoming version of emacs or elpy
 ;; https://github.com/jorgenschaefer/elpy/issues/887
 (setq python-shell-completion-native-enable nil)
