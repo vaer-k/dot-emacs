fe336acae0a6c62f48b749b210f1c6cd0b4dbdc7
Vincent Raerek <vincent@monadnomad.net>
2019-05-03 11:20:30 -0700

    Default to opaque

--- customizations/ui.el
+++ customizations/ui.el
@@ -59,19 +59,19 @@
 ;; no bell
 (setq ring-bell-function 'ignore)
 
-;; doom
+;; doom ------------------------------------------
 (load-theme 'doom-one t)
 (doom-modeline-mode 1)
 (doom-themes-neotree-config)
 (doom-themes-visual-bell-config)
 (doom-themes-org-config)
 
-;; Tranparency
+;; Tranparency ------------------------------------------
+
+(add-to-list 'default-frame-alist '(alpha . (100 . 100)))
+
 ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
 ;;(set-frame-parameter (selected-frame) 'alpha <both>)
-(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
-(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
-
 (defun toggle-transparency ()
   (interactive)
   (let ((alpha (frame-parameter nil 'alpha)))
@@ -82,7 +82,7 @@
                     ;; Also handle undocumented (<active> <inactive>) form.
                     ((numberp (cadr alpha)) (cadr alpha)))
               100)
-         '(85 . 50) '(100 . 100)))))
+         '(90 . 50) '(100 . 100)))))
 (global-set-key (kbd "C-c t") 'toggle-transparency)
 
 ;; Set transparency of emacs
