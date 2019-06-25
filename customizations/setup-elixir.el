341a01c1d2ffbed5e0ba53ccdcb1bfa44733f574
Vincent Raerek <vincent@monadnomad.dev>
2019-06-24 18:23:55 -0700

    Set web-mode indentation to 2

--- customizations/setup-elixir.el
+++ customizations/setup-elixir.el
@@ -3,4 +3,14 @@
           (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 
 (require 'web-mode)
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-indent-style 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
